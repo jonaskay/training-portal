ee4a1fb9ba602af2d85ef5fc5ade7092d5fe71db
Joonas Kykkänen <jonaskay@iki.fi>
2025-09-13 21:43:58 +0300

    Add support for markdown view files

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "pages#index"
+
+  get "about" => "pages#about"
 end
--- test/system/pages_test.rb
+++ test/system/pages_test.rb
@@ -6,4 +6,17 @@ class PagesTest < ApplicationSystemTestCase
 
     assert_selector "h1", text: "Welcome to Training Portal"
   end
+
+  test "visiting the about page renders markdown" do
+    visit about_path
+
+    assert_selector "h1", text: "About Our Company"
+    assert_selector "h2", text: "Our Mission"
+    assert_selector "h3", text: "Markdown Support"
+    assert_selector "strong", text: "training portal"
+    assert_selector "code"
+    assert_text "def hello_world"
+    assert_text "Current time is"
+    assert_text "Page title from front matter: About Us"
+  end
 end
